d4b5d58d0c42b03bfa94b8c88765d133f7d46103
harsuuu <harshjoshi7895@gmail.com>
2024-06-20 00:31:02 +0530

    operators in php

---
+++ operator.php
@@ -0,0 +1,24 @@
+<?php
+    $a=10;
+    $b=20;
+
+    //Arithmetic Operator
+    echo $a + $b;
+    echo "<br>"; // Corrected line break
+    echo $a - $b;
+    echo "<br>"; // Added line break for clarity
+    echo $a * $b;
+    echo "<br>"; // Added line break for clarity
+    echo $a / $b;
+    echo $a ** $b;
+    echo "<br>"; // Corrected line break
+    echo $a % $b;
+    echo "<br>"; // Added line break for clarity
+    echo $a * $b;
+    echo "<br>"; // Added line break for clarity
+    echo $a / $b;
+
+    //Assignment Operator
+    $a+=6;
+    echo $a;
+?>
