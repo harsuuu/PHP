f9d7a98ce0f2ef7b0366b8c04b49d268e37eb9cf
harsuuu <harshjoshi7895@gmail.com>
2024-06-20 00:32:06 +0530

    condition in php

---
+++ condition.php
@@ -0,0 +1,25 @@
+<?php
+    // $a=65;
+    // $b=9;
+
+    // if($a>78)
+    // {
+    //     echo "a is grater than 78";
+    // }    
+    // else{
+    //     echo "Not grater than 78";
+    // }
+
+    $age=28;
+
+    if($age>18)
+    {
+        echo "Can Vote";
+    }
+    elseif($age<18){
+        echo "can not vote";
+    }
+    else{
+        echo "no";
+    }
+?>
